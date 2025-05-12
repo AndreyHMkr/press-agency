44c995650654aa313ba946774470b817ae7fdd31
Andriy <grushevoyandrey@gmail.com>
2025-05-12 17:38:03 +0100

    chenged forms.py(all users can choose any) topic and models.py(name:unique fild)

--- press_agency/models.py
+++ press_agency/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class Topic(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     description = models.TextField(
         blank=True,
         null=True,
